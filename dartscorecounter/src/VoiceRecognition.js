1b39fa84e2ff767eeb08b1407180beb91831e32c
pawelsienkiewicz <phafor@gmail.com>
2021-02-10 21:36:30 +0100

    Counting average score works

--- dartscorecounter/src/VoiceRecognition.js
+++ dartscorecounter/src/VoiceRecognition.js
@@ -4,25 +4,45 @@ import SpeechRecognition, {
 } from "react-speech-recognition";
 
 const VoiceRecognition = () => {
-  const { transcript, resetTranscript } = useSpeechRecognition();
+  const {
+    transcript,
+    resetTranscript,
+    finalTranscript,
+  } = useSpeechRecognition();
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return alert("Your browser does not support Speech Recognition API!");
   }
 
+  var scores = finalTranscript
+    .split(" ")
+    .map(Number)
+    .filter((el) => el <= 180);
+
+  function AverageScore() {
+    let sum = 0;
+    for (let i = 0; i < scores.length; i++) {
+      sum += scores[i];
+    }
+    return sum / scores.length;
+  }
+
   return (
     <div>
       <button
-        onClick={SpeechRecognition.startListening({
-          continuous: true,
-          language: "pl",
-        })}
+        onClick={() =>
+          SpeechRecognition.startListening({
+            language: "pl",
+            continuous: true,
+          })
+        }
       >
         Start
       </button>
-      <button onClick={SpeechRecognition.abortListening}>Stop</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+      <p>{finalTranscript}</p>
+      <AverageScore />
     </div>
   );
 };
